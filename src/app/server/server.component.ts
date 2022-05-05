d1af616aa9b6319b20722501939a964254d537f4
ASHARALORAN96 <asharoran96@gmail.com>
2022-05-05 23:23:15 +0300

    create new component and the angular workflow

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+//Decorator
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
