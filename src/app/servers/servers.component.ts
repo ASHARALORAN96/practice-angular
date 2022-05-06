766b76d594ea7cddf779038bf35788cee406370b
ASHARALORAN96 <asharoran96@gmail.com>
2022-05-06 16:17:21 +0300

    practice data bindings and directives is much like an instactations in the DOM

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,17 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  allowDisabled : boolean = false
-  constructor() { 
-    setTimeout(()=>{
-      this.allowDisabled = true
-    },2000)
+  allowDisabled: boolean = false;
+  createNewServer: string = 'there is no server created';
+  inputData : string = ''
+  showNewServer = false
+  constructor() {
+    setTimeout(() => {
+      this.allowDisabled = true;
+    }, 2000);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addServer() {
+    this.showNewServer=true
+    this.createNewServer = 'server is created '+ this.inputData;
+  }
+  inputEventHandler(event : any){
+    this.inputData = (<HTMLInputElement>event.target).value;
   }
 
 }
